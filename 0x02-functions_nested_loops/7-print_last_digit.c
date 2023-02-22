7509025c41e7fbad877684c48ec3a285ab153f0c
Sherryivy <sherryivy17@gmail.com>
2023-02-22 04:26:44 -0800

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,14 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
 
-	if (ld < 0)
-		ld *= -1;
+	int last;
 
-	_putcahr(ld + '0');
-
-	return (0);
+	last = n % 10;
+	if (last < 0)
+	{
+	last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
 }
