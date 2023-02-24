151190bf802c7be4de88392f1b543d15ff156684
Sherryivy <sherryivy17@gmail.com>
2023-02-24 04:17:01 -0800

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,7 +6,9 @@
  */
 
 int main(void)
+
 {
+
 	int i;
 
 	printf("Infinite loop incoming : (\n");
@@ -15,6 +17,8 @@ int main(void)
 	/*{*/
 	/*	putchar(i);*/
 	/*}*/
+
 	printf("Infinite loop avoided! \\o/\n");
+	
 	return (0);
 }
