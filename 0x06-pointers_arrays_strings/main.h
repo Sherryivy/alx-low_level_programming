a9d667b718919737a7b9c1795a707f098ba1db7a
Sherryivy <sherryivy17@gmail.com>
2023-03-01 08:38:58 -0800

    Task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+#include <string.h>
 #ifndef MIAN_H
 #define MAIN_H
 
@@ -7,7 +8,7 @@ char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
 
-char *strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 
 int _strcmp(char *s1, char *s2);
 
